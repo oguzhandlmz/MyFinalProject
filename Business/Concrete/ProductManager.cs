9942a0ce52828763a00efb6ccb677d09a408087c
MONSTER <MONSTER@DESKTOP-326766T>
2021-11-03 20:20:13 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IproductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş kodları geçiyor varsaydık
+            //Yetkisi var mı??
+
+            return _productDal.GetAll();
+
+
+            
+
+
+        }
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    // PUBLİC - BU CLASSA DİĞER KATMANLARDA ULAŞABİLSİN DEMEKTİR.İNTERNAL - SADECE ENTİTİES ERİŞEBİLİR DEMEK YANİ DEFAULT U O DUR.
+    public class Product:IEntity 
+    {
+        public int ProductId { get; set; }
+        public int CatogoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    }
+}
